1d981921cec12fb979a76b0f1f4e20dd6eb29651
Anna Huang <annatointro@gmail.com>
2019-05-24 15:26:51 -0600

    add data

--- src/components/CoffeeEditor.vue
+++ src/components/CoffeeEditor.vue
@@ -1,12 +1,70 @@
 <template>
   <div class="CoffeeEditor">
-    <h2>coffee editor</h2>
+    <h3>coffee editor test 123</h3>
+    <div class="coffeeType">
+      <div class="num">00</div>
+      <div class="cupContainer">
+        <div class="cup">
+          Cup
+          <div class="ingradient">
+            <div class="coffee"></div>
+            <div class="milk"></div>
+            <div class="water"></div>
+            <div class="bubble"></div>
+          </div>
+        </div>
+      </div>
+      <div class="control">
+        <h2>Expresso</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      editing: true,
+      cupTypes: ["small", "medium", "big", "mock"],
+      coffees: [
+        {
+          name: "Expresso",
+          coffee: 80,
+          bubble: 0,
+          water: 0,
+          type: "small"
+        },
+        {
+          name: "Americano",
+          coffee: 40,
+          bubble: 0,
+          water: 40,
+          milk: 0
+        },
+        {
+          name: "Cappuccino",
+          coffee: 40,
+          bubble: 0,
+          water: 0,
+          milk: 50,
+          type: "big"
+        },
+        {
+          name: "Latte",
+          coffee: 30,
+          bubble: 30,
+          water: 0,
+          milk: 30
+        }
+      ],
+      favoriteType: "Cappuccino"
+    };
+  }
+};
 </script>
 
 <style lang="scss">
+.CoffeeEditor {
+}
 </style>
